ea4770e936a4be7fef9b0454dec45e6e4c804d4d
Daniele Monteleone <daniele.monteleone.it@gmail.com>
2018-08-17 23:56:41 +0200

    Update example

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,14 @@ fn main() {
     let mut trie = Trie::from_word("hello😝world".chars().collect());
     trie.insert("hella".chars().collect());
     trie.insert("hello😝man!".chars().collect());
+    trie.insert("hello".chars().collect());
 
-    println!("{:?}", trie);
+    println!("{:?}", trie.get("hell".chars().collect()).unwrap());
+
+    println!(
+        "{:?}",
+        trie.iter()
+            .map(|vec| vec.iter().collect::<String>())
+            .collect::<Vec<String>>()
+    );
 }
